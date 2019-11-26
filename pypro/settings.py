1025206927489535fd3cb2886d94f46fc564b845
aguiardafa <diego.fernandes.aguiar@gmail.com>
2019-11-26 06:14:12 -0300

    Configurando codecov para ignorar configuração do AWS para efeito de cobertura de testes

--- pypro/settings.py
+++ pypro/settings.py
@@ -138,7 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 COLLECTFAST_ENABLED = False
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
-if AWS_ACCESS_KEY_ID:
+if AWS_ACCESS_KEY_ID:  # pragma: no cover
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
